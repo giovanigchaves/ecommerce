05c5d430e3fe2ad303d4019d3723a331a863318d
Giovani G Chaves <giovanigchaves.engenharia@gmail.com>
2018-04-22 15:50:20 -0300

    criamos a classe Page

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-<?php 
+<?php
 
-require_once("vendor/autoload.php");
+    require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+    use \Slim\Slim;
+    use \Hcode\Page;
 
-$app->config('debug', true);
+    $app = new Slim();
 
-$app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
+    $app->config('debug', true);
 
-	$results = $sql->select("SELECT * FROM tb_users");
+    //rota
+    $app->get('/', function()
+    {
 
-	echo json_encode($results);
+        $page = new Page();
 
-});
+        $page->setTpl("index");
 
-$app->run();
+    });
 
- ?>
+    $app->run();
+
+?>
---
+++ views-cache/footer.eee081e367a51fa4dc8b018f24306629.rtpl.php
@@ -0,0 +1,22 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>
+
+
+
+
+<h2>footer</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
